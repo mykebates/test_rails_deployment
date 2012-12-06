c1d4d21d373e5c5042d3d1b169c2b7510bde721d
Myke Bates <myke@uptrending.com>
2012-12-06 09:09:20 -0500

    home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 FirstApp::Application.routes.draw do
+  get "home/index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -30,7 +31,7 @@ FirstApp::Application.routes.draw do
   #     resources :comments, :sales
   #     resource :seller
   #   end
-
+ 
   # Sample resource route with more complex sub-resources
   #   resources :products do
   #     resources :comments
@@ -48,7 +49,7 @@ FirstApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
